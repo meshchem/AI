08ab782fec060c124f4f46c86a9f2bc4555e2271
meshchem <meshchem@tcd.ie>
2026-02-17 17:29:56 +0000

    implemented astar search alg - manhattan and euclidean

--- run_solvers.py
+++ run_solvers.py
@@ -1,6 +1,7 @@
 from src.maze_generator import load_maze, plot_maze
 from src.dfs import dfs
 from src.bfs import bfs
+from src.astar import astar_manhattan, astar_euclidean
 
 
 mazes = {
@@ -19,5 +20,19 @@ for size, maze in mazes.items():
 print("\nBFS: ")
 for size, maze in mazes.items():
     result = bfs(maze)
+    # plot_maze(maze.grid, maze.start, maze.goal, path=result.path)
+    print(f"  {size} | path length: {result.path_length} | nodes explored: {result.nodes_explored}")
+
+
+
+print("\nA* Manhattan: ")
+for size, maze in mazes.items():
+    result = astar_manhattan(maze)
+    # plot_maze(maze.grid, maze.start, maze.goal, path=result.path)
+    print(f"  {size} | path length: {result.path_length} | nodes explored: {result.nodes_explored}")
+
+print("\nA* Euclidean:")
+for size, maze in mazes.items():
+    result = astar_euclidean(maze)
     # plot_maze(maze.grid, maze.start, maze.goal, path=result.path)
     print(f"  {size} | path length: {result.path_length} | nodes explored: {result.nodes_explored}")
